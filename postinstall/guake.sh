24538009ab261ae7017289f7824050582602e9ee
Pascal Betting <pascal.betting@gmx.de>
2021-01-16 21:23:57 +0100

    refactor(config): adjust guake keybinding config
    closes #2

--- postinstall/guake.sh
+++ postinstall/guake.sh
@@ -5,10 +5,10 @@ dconf write /apps/guake/style/font/palette "'#1C1C1D1D1919:#D0D01B1B2424:#A7A7D3
 dconf write /apps/guake/style/font/palette-name "'Monokai'"
 dconf write /apps/guake/style/background/transparency 90
 dconf write /apps/guake/keybindings/global/show-hide "'F10'"
-dconf write /apps/guake/keybindings/local/reset-terminal "'<Alt>r'"
-dconf write /apps/guake/keybindings/local/quit "'<Alt>q'"
 dconf write /apps/guake/keybindings/local/new-tab "'<Alt>t'"
 dconf write /apps/guake/keybindings/local/close-tab "'<Alt>w'"
+dconf write /apps/guake/keybindings/local/previous-tab "'<Alt>Left'"
+dconf write /apps/guake/keybindings/local/next-tab "'<Alt>Right'"
 dconf write /apps/guake/keybindings/local/rename-current-tab "'<Alt>r'"
 dconf write /apps/guake/keybindings/local/clipboard-copy "'<Alt>c'"
 dconf write /apps/guake/keybindings/local/clipboard-paste "'<Alt>v'"
