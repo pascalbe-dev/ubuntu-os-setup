c9050947be8e1356c804fc1050f79224c08e50f2
Pascal Betting <pascal.betting@gmx.de>
2021-01-16 21:31:11 +0100

    refactor(home files): do not set the git config as symlink
    + because you have to change it after installation
    + add note to change it

--- postinstall/git.sh
+++ postinstall/git.sh
@@ -3,3 +3,5 @@
 cat /dev/zero | ssh-keygen -q -N "" >/dev/null
 cat $HOME/.ssh/id_rsa.pub | xclip -sel clip
 read -p "Copy your SSH key to the services, which require it (github, other git servers). It is already in your clipboard. Then press Enter"
+cp $(pwd)/postinstall/data/.gitconfig ~/
+read -p "Adjust the .gitconfig file in your home directory to match your email address and name. Then press Enter"
