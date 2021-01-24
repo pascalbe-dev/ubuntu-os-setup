f436b2b0528d0dc969c9cf5c36b01d2f92ea158d
Pascal Betting <pascal.betting@gmx.de>
2021-01-24 15:51:33 +0100

    feat: add todoist ulauncher extension

--- postinstall/ulauncher.sh
+++ postinstall/ulauncher.sh
@@ -2,3 +2,6 @@
 
 ln -sf $(pwd)/postinstall/data/settings.json $HOME/.config/ulauncher/
 ln -sf $(pwd)/postinstall/data/extensions.json $HOME/.config/ulauncher/
+
+# todoist extension
+pip3 install todoist-python
