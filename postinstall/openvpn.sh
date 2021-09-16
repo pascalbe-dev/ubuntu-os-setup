8b09103e4666281576813b0c0058c679284ab5f3
Pascal Betting <pascal.betting@gmx.de>
2021-09-16 17:55:50 +0200

    feat: simplify vpn connection and make settings persistent

--- postinstall/openvpn.sh
+++ postinstall/openvpn.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-read -p "Download your vpn file (.ovpn file) to the Downloads directory. Then press Enter"
-sudo mv $HOME/Downloads/*.ovpn $HOME/Documents/work.vpn
-sudo cat $(pwd)/postinstall/data/vpn-extension.txt >>$HOME/Documents/work.vpn
+read -p "Download your vpn file (.ovpn file) to the Downloads directory. Edit it, if required. Then press Enter"
+sudo mv $HOME/Downloads/*.ovpn $HOME/Documents/work.ovpn
+sudo nmcli connection import type openvpn file $HOME/Documents/work.ovpn
+# prioritize standard connection so we can still access the normal internet
+nmcli connection modify work ipv4.route-metric 1000
+echo "Done. You can now add your username (and password) to the openvpn connection via the VPN settings."
