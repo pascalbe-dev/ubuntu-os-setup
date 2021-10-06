5f4cfbc689393d4d0970a623a1329b42b19dde6e
Pascal Betting <pascal.betting@gmx.de>
2021-10-06 08:30:38 +0200

    docs: add hint about SSL

--- postinstall/ssl.sh
+++ postinstall/ssl.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# see more here: https://askubuntu.com/a/649463
 # SSL certificates can be retrieved via the browser (by exporting from the website via the lock symbol)
 read -p "Download the ca certificates to your downloads folder. Make sure, they end with .crt (Ubuntu does not recognize all endings). Then press Enter"
 
