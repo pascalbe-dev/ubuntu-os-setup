bd50fb0a8ba8a795beed513fe4f4b2af351431cc
Firstname Lastname <name@domain.de>
2022-03-05 21:48:26 +0100

    fix(postinstall): ulauncher config
    + it failed, when the folder was not there

--- postinstall/ulauncher.sh
+++ postinstall/ulauncher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir -p $HOME/.config/ulauncher
 ln -sf $(pwd)/postinstall/data/settings.json $HOME/.config/ulauncher/
 ln -sf $(pwd)/postinstall/data/extensions.json $HOME/.config/ulauncher/
 
