3a6838892431ff35765eb5a4971d03d63f5bb521
Pascal Betting <pascal.betting@gmx.de>
2022-03-06 10:59:54 +0100

    docs(install): add hint about certificates for the browser

--- postinstall/ssl.sh
+++ postinstall/ssl.sh
@@ -2,9 +2,12 @@
 
 # see more here: https://askubuntu.com/a/649463
 # SSL certificates can be retrieved via the browser (by exporting from the website via the lock symbol)
+# or they might be shared by the organization
 read -p "Download the ca certificates to your downloads folder. Make sure, they end with .crt (Ubuntu does not recognize all endings). Then press Enter"
 
 sudo mkdir -p /usr/local/share/ca-certificates/custom
 sudo mv $HOME/Downloads/*.crt /usr/local/share/ca-certificates/custom/
 
 sudo update-ca-certificates
+
+read -p "Now import the certificates under '/usr/local/share/ca-certificates/custom/' to the browser. Then press Enter"
