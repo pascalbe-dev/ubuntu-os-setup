7af6d402a77d9291419019fc956330bfe2b3fc5d
Pascal Betting <pascal.betting@gmx.de>
2022-08-09 14:31:24 +0200

    feat: add edge for web dev; cleanup zshenv

--- roles/ubuntu-base/files/configuration/ohmyzsh/.zshenv.sh
+++ roles/ubuntu-base/files/configuration/ohmyzsh/.zshenv.sh
@@ -1,14 +1,5 @@
 # put custom shell init scripts, aliases, whatever here
-
-export PATH=$PATH:$HOME/ubuntu-os-setup/scripts/
-
-export SSH_USER_1="ssh user 1"
-export SSH_HOST_1="ssh host 1"
-
 export DIRECTORY_1="$HOME/repos/work/"
-
-export GITHUB_USER_1=
-export GITHUB_USER_2=
 
 # commands to open tools
 export COMMAND_VSCODE="code-insiders"
