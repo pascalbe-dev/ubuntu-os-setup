2a82f425cd6a2eded4e7d9bf5f510a0c743c62fd
Pascal Betting <pascal.betting@gmx.de>
2024-01-31 08:58:21 +0100

    fix: add the zshenv file

--- roles/ubuntu-base/files/configuration/ohmyzsh/.zshenv.sh
+++ roles/ubuntu-base/files/configuration/ohmyzsh/.zshenv.sh
@@ -4,6 +4,7 @@ export DIRECTORY_1="$HOME/repos/work/"
 # commands to open tools
 export COMMAND_VSCODE="code-insiders"
 export COMMAND_FINDER="xdg-open"
+export COMMAND_INTELLIJ="intellij-idea-ultimate"
 export COMMAND_BROWSER_OPEN="google-chrome --app-url"
 
 export GITHUB_USER_1="pascalbe-dev"
