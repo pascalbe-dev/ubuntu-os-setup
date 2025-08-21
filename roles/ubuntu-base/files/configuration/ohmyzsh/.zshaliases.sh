d877e3d72218e9f4f261c64283441dbf8f16d65f
Pascal Betting <pascal.betting@gmx.de>
2025-08-21 16:05:52 +0200

    feat: add command to update the aliases in this repo

--- roles/ubuntu-base/files/configuration/ohmyzsh/.zshaliases.sh
+++ roles/ubuntu-base/files/configuration/ohmyzsh/.zshaliases.sh
@@ -1,6 +1,7 @@
 # zsh
 alias oms='vim ~/.zshaliases'
 alias ome='vim ~/.zshenv'
+alias omcf='cp $HOME/.zshaliases $HOME/ubuntu-os-setup/roles/ubuntu-base/files/configuration/ohmyzsh/.zshaliases.sh'
 alias omc="background $COMMAND_INTELLIJ $HOME/ubuntu-os-setup"
 # reset shell (after changing aliases or stuff like that)
 alias res='exec zsh -l && clear'
